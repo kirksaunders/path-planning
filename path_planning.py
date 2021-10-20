93668c0727b3023b53d59777470dd3f811e07e58
kirksaunders <kirksaunders12@gmail.com>
2021-10-19 23:48:11 -0400

    Added replaybuffer (needs testing)

--- path_planning.py
+++ path_planning.py
@@ -1,13 +1,84 @@
 from env import PathPlanningEnv
+import numpy as np
+import copy
+
+class ReplayBuffer:
+    def __init__(self, capacity, state_shapes):
+        assert capacity > 0
+        self.capacity = capacity
+        self.size = 0
+        self.insert_index = 0
+
+        assert(type(state_shapes) is list)
+
+        self.state_arrays = [None] * len(state_shapes)
+        for i in range(0, len(state_shapes)):
+            if type(state_shapes[i]) is tuple:
+                shape = (capacity, *(state_shapes[i]))
+            else:
+                shape = (capacity, state_shapes[i])
+            self.state_arrays[i] = np.empty(shape, dtype=np.float32)
+
+        self.actions = np.empty(capacity, dtype=np.int32)
+        self.rewards = np.empty(capacity, dtype=np.float32)
+        self.terminal = np.empty(capacity, dtype=bool)
+        self.next_state_arrays = copy.deepcopy(self.state_arrays)
+
+    def add(self, states, action, reward, next_states, terminal):
+        assert(type(states) is list)
+        assert(type(next_states) is list)
+        assert(len(states) == len(self.state_arrays))
+        assert(len(next_states) == len(self.next_state_arrays))
+
+        for i in range(0, len(states)):
+            self.state_arrays[i][self.insert_index, ...] = states[i]
+        self.actions[self.insert_index] = action
+        self.rewards[self.insert_index] = reward
+        self.terminal[self.insert_index] = terminal
+        for i in range(0, len(next_states)):
+            self.next_state_arrays[i][self.insert_index, ...] = next_states[i]
+
+        self.insert_index += 1
+        if self.insert_index >= self.capacity:
+            self.insert_index = 0
+
+        if self.size < self.capacity:
+            self.size += 1
+
+    def mini_batch(self, size):
+        assert(self.size >= size)
+
+        # Allocate space for return batch
+        state_arrays = [None] * len(self.state_arrays)
+        for i in range(0, len(self.state_arrays)):
+            state_arrays[i] = np.empty((size, *(self.state_arrays[i][0].shape)), dtype=np.float32)
+        actions = np.empty(size, dtype=np.int32)
+        rewards = np.empty(size, dtype=np.float32)
+        terminal = np.empty(size, dtype=bool)
+        next_state_arrays = copy.deepcopy(state_arrays)
+
+        # Select samples
+        selected = np.random.choice(self.size, size)
+        for i in range(0, len(selected)):
+            for j in range(0, len(state_arrays)):
+                state_arrays[j][i] = self.state_arrays[j][selected[i]]
+                next_state_arrays[j][i] = self.next_state_arrays[j][selected[i]]
+            actions[i] = self.actions[selected[i]]
+            rewards[i] = self.rewards[selected[i]]
+            terminal[i] = self.terminal[selected[i]]
+
+        return state_arrays, actions, rewards, terminal, next_state_arrays
 
 def main():
-    env = PathPlanningEnv("grid1.bmp")
-    state = env.reset((10, 10), (3, 3))
-    while True:
-        print(state)
-        action = int(input())
-        state, reward, terminal = env.step(action)
-        print(reward, terminal)
+    rb = ReplayBuffer(50, [(128, 64, 3), (2)])
+
+    #env = PathPlanningEnv("grid1.bmp")
+    #state = env.reset((10, 10), (3, 3))
+    #while True:
+        #print(state)
+        #action = int(input())
+        #state, reward, terminal = env.step(action)
+        #print(reward, terminal)
 
 if __name__=='__main__':
     main()
